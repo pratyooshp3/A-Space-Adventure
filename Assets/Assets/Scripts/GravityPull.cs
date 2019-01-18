12748dac2f076f5990c6dfa08193644d097aa117
BatesJackson <32694371+BatesJackson@users.noreply.github.com>
2019-01-18 08:12:14 -0800

    Actually added Camera Shake script and deleted extra library folder

---
+++ Assets/Assets/Scripts/GravityPull.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPull : MonoBehaviour
+{
+
+    Vector3 originPosition;
+    Quaternion originRotation;
+
+    public Light redLight; // Add desired red light in inspector
+
+    //Camera Shake
+    float shake_intensity;  // Intensity of Camera Shake
+
+    //Light Flash
+    float flashRate = 1.0f;  //How fast light flashes
+    float canFlash = 0.0f;
+    Coroutine flasher = null;
+
+
+    public bool inPull;
+    private void Start()
+    {
+        //redLight.gameObject.SetActive(false);
+        inPull = false;
+    }
+
+
+    void Shake()
+    {
+        if (inPull)
+        {
+            originPosition = transform.position;
+            originRotation = transform.rotation;
+            shake_intensity = .0012f;
+            transform.position = originPosition + Random.insideUnitSphere * shake_intensity;
+        }
+    }
+    /*IEnumerator Flash()
+    {
+        while (true)
+        {
+            if (redLight.gameObject.activeSelf)
+            {
+                redLight.gameObject.SetActive(false);
+            }
+            else
+            {
+                redLight.gameObject.SetActive(true);
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+    }*/
+
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.CompareTag("GravPull"))
+        {
+            Shake();
+        }
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("GravPull"))
+        {
+            inPull = true;
+            //flasher = StartCoroutine(Flash());
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("GravPull"))
+        {
+            inPull = false;
+            //StopCoroutine(flasher);
+        }
+    }
+}
