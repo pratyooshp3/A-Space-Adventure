e6291b680f23a778f8b9fbecc414505d667ddd67
BatesJackson <bates677@gmail.com>
2019-01-18 13:42:49 -0800

    Navigation added

---
+++ Assets/Assets/Scripts/PathNavigation.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PathNavigation : MonoBehaviour
+{
+    public Transform[] marsPath;
+    public Transform[] jupiterPath;
+    public GameObject finish;
+
+    Vector3 direction;
+
+    private bool buttonActivated;
+
+
+    float rotateSpeed = 2.0f;
+
+
+    void Start()
+    {
+        iTween.Init(this.gameObject);
+        tween(marsPath);
+    }
+    private void Update()
+    {
+
+        
+    }
+
+
+    /*void OnGUI () {
+		if(buttonActivated){
+			if(GUILayout.Button ("Reset")){
+				reset();
+				tween();
+			}
+		}
+	}*/
+
+    void OnDrawGizmos()
+    {
+        iTween.DrawPath(marsPath);
+    }
+
+    void tween(Transform[] navPath)
+    {
+        float time = 0.0f;
+        if(navPath == marsPath)
+        {
+            time = 20.0f;
+        }
+        iTween.MoveTo(gameObject, iTween.Hash("path", navPath, "time", time, "orienttopath", true, "looktime", .9, "easetype", "easeInOutSine", "oncomplete", "complete"));
+       // iTween.MoveTo(gameObject, iTween.Hash("path", navPath, "time", time,  "easetype", "easeInOutSine", "oncomplete", "complete"));
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("planet"))
+        {
+            iTween.Pause();
+        }
+    }
+    
+        
+    
+    /*void reset(){
+		buttonActivated=false;
+		transform.position=new Vector3(0,0,0);
+		transform.eulerAngles=new Vector3(0,0,0);
+	}*/
+
+    void complete()
+    {
+        buttonActivated = true;
+    }
+}
