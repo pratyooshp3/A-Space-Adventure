fea6cd72a927924c58d8dad7d6698a8fb2e802a4
PChandra <padma.chandra82@gmail.com>
2019-01-19 11:35:25 -0800

    Checkin of UI noticeboard template and tilt rotation script

---
+++ Assets/Assets/Scripts/NoticeBoard_VR.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class NoticeBoard_VR : MonoBehaviour {
+
+	// Use this for initialization
+	public string Destination; 
+	public string craftname;
+
+	public Button currentStatus; //default setting
+	public Button NextStatus;
+
+	public Text infoBox;
+	public Text MenuTitle; 
+	public Text WarningBox;
+
+	private int currentcount =1;
+	private int nextcount =1;
+
+
+	private Dictionary<int,string> statusMap;
+	private Dictionary<int,string> warningMap;
+	private Dictionary<int,string> instrMap;
+
+		void Start() {
+			
+		    Destination = "Planet IDA";
+		    craftname="USS-Enterprise";
+		    currentStatus.name = "Current Status";
+		    NextStatus.name = "Next Instruction";
+		   // Initlaize Dictionary
+			statusMap = new Dictionary<int,string>{ 
+				{1,"Boarding ISS...."},{2,"Boarding Spaceship: "+craftname+"...."},{3,"Starting your mission to " +Destination},
+				{4, "Approaching Mars....."},{5, "Approaching Asteriod Belt,Enabling AutoPilot to avoid collision"},
+				{6,"Approaching Jupiter....."},
+				{7, "WARNING !! WARNING !! "+craftname+" Out Of Control. AutoPilot Not Operable!!"},
+				{8, "New path calculated.Restoring Control of craft to Pilot."},{9," Approaching Saturn,Uranus,Neptune"},
+				{10,"Approaching Pluto and edge of Solar System.Auto Pilot Restored"}
+			};
+		  
+			warningMap= new Dictionary<int,string>{ 
+				{1,"zero gravity Floating. Nothing to worry about"},{3,"Possible Anamolies detected on your path to "+Destination},
+				{6,"Warning: AutoPilot Failing...."},
+				{7,"Hit the big RED Button for help.\n"+craftname+" entering atmosphere in 5 minutes."}
+			};
+
+			instrMap = new Dictionary<int,string> { 
+				{1,"Access TelePort Point Closest to you to Board ISS. "},{2,"Checking Craft Integrity..."},
+				{3,"Integrity Check Complete.Please Proceed with Caution."},
+				{6,"Pilot must take over control,if Autopilot fails."},{7,"!!!Hit the big RED Button for help!!!"},
+
+			};
+			
+		   //Set Default Texts
+			MenuTitle.text= craftname+" : FlightPlan\n\n"+ "Deliver critical Equipments to "+ Destination +
+			               " located at the edge of MilkyWay Galaxy."; // Mission Statement
+
+		    infoBox.text = "\n Welcome Aboard " + craftname + ".\n" + "You will be notified with further instructions";
+		    WarningBox.text="";
+
+			//Set Font,Font size and Color.
+		    MenuTitle.color=Color.cyan;
+			infoBox.color = Color.green;
+			WarningBox.color=Color.yellow;
+
+
+			//Add Listeners		   
+			currentStatus.onClick.AddListener(()=>GetCurrentStatus());
+			NextStatus.onClick.AddListener(()=>FetchNextInstruction());
+		}
+
+		void update() {}
+
+	   /* Needs to be called by external GAme Object like SpaceCraft when it reaches a particular Marker
+	    * REfer to statusMap initalization for status information 
+	    * which will give you an idea at what markers,this method needs to be called 
+	    */ 
+		public void UpdateCurrentStatus(){
+		if(currentcount<= statusMap.Count)  currentcount++;
+			GetCurrentStatus();
+
+		}
+
+		public void ResetSceneStatus(int resetToindex){
+			if( !statusMap.ContainsKey(resetToindex) )
+			{
+				Debug.Log("NoticeBoard.ResetSceneStatus():Invalid Reset Index assigned ="+ resetToindex);
+				Debug.Log("Index must be in range 1 to "+statusMap.Count);
+				return;
+			} 
+			currentcount = resetToindex;
+			GetCurrentStatus();
+
+		}
+
+	    // Called when NextStatus button is clicked. 
+		private void FetchNextInstruction() {
+
+			while(nextcount<=instrMap.Count)
+			{
+				nextcount++;
+				if(instrMap.ContainsKey(nextcount)) {
+					if(nextcount==7) 
+						infoBox.color=Color.red;
+					else
+						infoBox.color=Color.green;
+					infoBox.text = "\n"+"INSTRUCTION: "+ instrMap[nextcount];
+
+					return;
+				}
+			}
+
+
+		}
+	   
+	   // Called when Current Status button is clicked.
+	private void GetCurrentStatus() {
+			nextcount = currentcount;
+			string instruction="";
+
+		  if(currentcount>statusMap.Count) {
+				Debug.Log("No more status Updates available");
+				infoBox.text = "\n STATUS: No Further anamolies detected on your path. Enjoy Rest of your journey to "+Destination;
+				return;
+		    }
+			if (!instrMap.ContainsKey(nextcount))
+			instruction = "No immediate threats detected.No action needs to be taken.";
+			else 
+				instruction = instrMap[nextcount];
+
+			if(nextcount==7) {
+				infoBox.color=Color.red;
+				WarningBox.color=Color.red;
+			}
+			else{
+				infoBox.color=Color.green;
+				WarningBox.color=Color.yellow;
+			}
+			infoBox.text = "\n"+"STATUS: "+statusMap[currentcount]+"\n\n"+"INSTRUCTION: "+instruction;
+
+		 	if (!warningMap.ContainsKey(currentcount))
+				WarningBox.text = "";
+			else
+				WarningBox.text = warningMap [currentcount];
+		}
+
+		
+
+
+
+}
---
+++ Assets/Assets/Scripts/planet_rotation_tilt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planet_rotation_tilt : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float tiltAngleZ=30.0f;
+	public float rotateSpeed=25.0f;
+
+	void Start () {
+
+		Debug.Log (" Tilt Angle along Z: " + tiltAngleZ);
+		Debug.Log (" Planet Rotation Speed: " + rotateSpeed);
+		transform.rotation=Quaternion.Euler (0.0f,0.0f,tiltAngleZ);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		transform.Rotate(transform.up*rotateSpeed*Time.deltaTime,Space.World);
+
+
+	}
+}
+
