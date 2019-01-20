630a6f44ca515bda2db557253e05fd6ea3af1f17
GrahamFord-Babbel <33167222+GrahamFord-Babbel@users.noreply.github.com>
2019-01-20 17:09:04 -0500

    1.20 Update: SpaceHelmet + ShipBooster + ShipControls
    -All of these updates are found in Assets>Assets>Scenes>BuildingScene
    -Everything appears to work (but my Oculus headset has been having issues, so couldn't test through that...simply had to manually change the X & Y of the "joystick" on the script to see if it works)
    -Only problem is that I had to turn off the OVRPlayerController script becauses it was poorly reacting to ShipBoost Script (was forcing the camera away from the ship)
    -Oculus Integration installed
    -Will update this for SteamVR (but for testing purposes used Oculus to start)

---
+++ Assets/Assets/Scripts/Graham Scripts - Gravity etc/CameraJoystickRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class CameraJoystickRotate : MonoBehaviour {
+
+    public Camera spaceCamera;
+    public float speed = 1;
+    public Vector2 oculusJoystick;
+    public Vector2 steamTouchPad;
+
+
+    // Use this for initialization
+    void Start () {
+        oculusJoystick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        steamTouchPad = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //make camera look at the pivot object (spaceship?)
+        spaceCamera.transform.LookAt(transform);
+        transform.Rotate(Vector3.up * oculusJoystick.y * speed * Time.deltaTime);
+        transform.Rotate(Vector3.right * oculusJoystick.x * speed * Time.deltaTime);
+
+
+
+    }
+}
---
+++ Assets/Assets/Scripts/Graham Scripts - Gravity etc/ShipBooster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBooster : MonoBehaviour {
+
+    public float speed;
+    //public float newZPosition = 1;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //first failed attempt to boost
+        //newZPosition = newZPosition * speed * Time.deltaTime;
+        //transform.position = new Vector3(0, 0, newZPosition);
+
+        rb.AddForce(transform.forward * speed);
+    }
+}
