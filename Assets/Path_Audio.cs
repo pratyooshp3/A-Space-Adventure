b13d9281f794a16e9a950b85e29bedeb747d39d8
PChandra <padma.chandra82@gmail.com>
2019-01-31 09:00:03 -0800

    Fixed Bugs in NoticeBoard Script

--- Assets/Path_Audio.cs
+++ Assets/Path_Audio.cs
@@ -3,53 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Path_Audio : MonoBehaviour {
-    public AudioClip[] audioFiles;
-    AudioSource audio;
+	public AudioClip[] audioFiles;
+	AudioSource audio;
 	public NoticeBoard_VR updateUI;
-    // Use this for initialization
-    void Start () {
-        StartCoroutine(EarthAndMoon());
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(EarthAndMoon());
 	}
 
-    private void OnTriggerEnter(Collider col)
-    {
+	private void OnTriggerEnter(Collider col)
+	{
 
-        if (col.gameObject.name == "MarsAudioTrigger")
-        {
-			updateUI.UpdateCurrentStatus();// UI Update
-            audio.clip = audioFiles[2];
-            audio.Play();
-        }
-        else if (col.gameObject.name == "TeslaAudioTrigger")
-        {
-			updateUI.UpdateCurrentStatus();// UI Update
-            audio.clip = audioFiles[3];
-            audio.Play();
-        }
-        else if (col.gameObject.name == "JupiterAudioTrigger")
-        {
-			updateUI.UpdateCurrentStatus();// UI Update
-            audio.clip = audioFiles[4];
-            audio.Play();
-        }
-        else if (col.gameObject.name == "Jupiter")
-        {
-			updateUI.UpdateCurrentStatus();// UI Update
-            audio.clip = audioFiles[5];
-            audio.Play();
-        }
-		updateUI.UpdateCurrentStatus();// UI Update
-    }
-    IEnumerator EarthAndMoon()
-    {
-        audio = GetComponent<AudioSource>();
-		updateUI.UpdateCurrentStatus();// UI Update
-        audio.clip = audioFiles[0];
-        audio.Play();
+		if (col.gameObject.name == "MarsAudioTrigger") {
+			updateUI.UpdateCurrentStatus ();// UI Update
+			audio.clip = audioFiles [2];
+			audio.Play ();
+		} else if (col.gameObject.name == "TeslaAudioTrigger") {
+			updateUI.UpdateCurrentStatus ();// UI Update
+			audio.clip = audioFiles [3];
+			audio.Play ();
+		} else if (col.gameObject.name == "JupiterAudioTrigger") {
+			updateUI.UpdateCurrentStatus ();// UI Update
+			audio.clip = audioFiles [4];
+			audio.Play ();
+		} else if (col.gameObject.name == "Jupiter") {
+			updateUI.UpdateCurrentStatus ();// UI Update
+			audio.clip = audioFiles [5];
+			audio.Play ();
 
-        yield return new WaitForSeconds(audio.clip.length + 1.0f);
+
+		} 
+			
+
+	}
+	IEnumerator EarthAndMoon()
+	{
+		audio = GetComponent<AudioSource>();
 		updateUI.UpdateCurrentStatus();// UI Update
-        audio.clip = audioFiles[1];
-        audio.Play();
-    }
+		audio.clip = audioFiles[0];
+		audio.Play();
+
+		yield return new WaitForSeconds(audio.clip.length + 1.0f);
+		updateUI.UpdateCurrentStatus();// UI Update
+		audio.clip = audioFiles[1];
+		audio.Play();
+	}
 }
